7025033f3a4e1c4202febd5b181a32701cdd4ff6
Eldor <kot63328@gmail.com>
2025-10-09 12:49:57 +0300

    add: about section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,32 @@
+import './About.css'
+
+const About = () => {
+    return (
+        <section className='abaut'>
+            <div className='container'>
+                <div className='about__wrapper'>
+                    <h2 className='about__title'>OUR STORY</h2>
+                    <div className='about__content'>
+                        <div className='about__content-info'>
+                            <h3 className='about__content-title'>Founded in 2011 by John Mathew Smith, <span>Konstruct</span> has become the number one construction management firm</h3>
+                            <p className='about__content-desc'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.</p>
+                        </div>
+
+                        <ul className="about__content-list">
+                            <li className='about__content-item'>
+                                <h3 className='about__content-subtitle'>WE PLAN</h3>
+                            </li>
+                            <li className='about__content-item'>
+                                <h3 className='about__content-subtitle'>WE MANAGE</h3>
+                            </li>
+                            <li className='about__content-item'>
+                                <h3 className='about__content-subtitle'> WE BUILD</h3>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default About
