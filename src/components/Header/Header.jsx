06993a522944fbeb6b96eff680983c52e56f7680
Eldor <kot63328@gmail.com>
2025-10-21 12:59:49 +0300

    feat: header responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,17 @@
+import { useState } from 'react'
+import { Menu, X } from 'lucide-react'
 import './Header.css'
 
 function Header() {
+    const [isMobile, setIsMobile] = useState(false)
+
+    const toggleMobileMenu = () => {
+        setIsMobile(prev => !prev)
+      }
+    
+
+
+
   return (
    <header className='header'>
     <div className='container'>
@@ -32,6 +43,40 @@ function Header() {
                     </li>
                 </ul>
             </nav>
+
+           {/* Mobile */}
+          <div className='header__mobile'>
+            <button
+              className='header__mobile-btn'
+              type='button'
+              onClick={toggleMobileMenu}
+            >
+              {isMobile ? <X className='close' /> : <Menu className='menu' />}
+            </button>
+
+            <nav className={`header__mobile-nav ${isMobile ? 'active' : ''}`}>
+              <ul className="header__mobile-list">
+                <li className="header__mobile-item">
+                  <a className="header__mobile-link" href="#">Home</a>
+                </li>
+                <li className="header__mobile-item">
+                  <a className="header__mobile-link" href="#">About</a>
+                </li>
+                <li className="header__mobile-item">
+                  <a className="header__mobile-link" href="#">Services</a>
+                </li>
+                <li className="header__mobile-item">
+                  <a className="header__mobile-link" href="#">Work</a>
+                </li>
+                <li className="header__mobile-item">
+                  <a className="header__mobile-link" href="#">Contact</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+
+
         </div>
     </div>
    </header>
